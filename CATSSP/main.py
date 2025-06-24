16ba3a7678761cfd1d8bcf5d2bfda314effd2d87
s7n5c4n <cr.hachim2.0@gmail.com>
2025-06-23 22:45:03 -0400

    Add TSP and PAR files for CGL problem and refactor main.py for file generation
    - Created new TSP file (cgl_70b.tsp) with problem data and edge weights.
    - Added new PAR file (cgl_70b.par) specifying problem file, output tour file, and number of runs.
    - Refactored main.py to read from a .catsp file and generate corresponding .tsp and .par files.
    - Removed unused cost matrix simulation and plotting code for clarity.

--- CATSSP/main.py
+++ CATSSP/main.py
@@ -1,49 +1,23 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+import os
 
-# Simulación de matriz de costos (reemplazar por lectura de .catsp real)
-# -1 indica transición prohibida
-C = np.array([
-    [-1, 300, 200, 654],
-    [305, -1, 963, 117],
-    [952, 493, -1, 704],
-    [678, 123, 951, -1]
-])
+catsp_path = r'C:\Users\crhac\CATSSP\CATSSP\CGL\cgl_114.catsp'
+tsp_path = catsp_path.replace('.catsp', '.tsp')
+par_path = catsp_path.replace('.catsp', '.par')
 
-n = C.shape[0]
-nodos = list(range(n))
+with open(catsp_path, 'r') as f_in, open(tsp_path, 'w') as f_out:
+    for line in f_in:
+        if 'DIMENSION' in line:
+            f_out.write(line)
+        elif 'EDGE_WEIGHT_SECTION' in line:
+            f_out.write('EDGE_WEIGHT_SECTION\n')
+        else:
+            f_out.write(line)
 
-solucion = []
-actual = 0
-visitados = {actual}
-solucion.append(actual)
+print(f'Se generó {tsp_path}')
 
-while len(visitados) < n:
-    costos_permitidos = [(j, C[actual, j]) for j in nodos if j not in visitados and C[actual, j] != -1]
-    if not costos_permitidos:
-        break
-    siguiente = min(costos_permitidos, key=lambda x: x[1])[0]
-    solucion.append(siguiente)
-    visitados.add(siguiente)
-    actual = siguiente
+with open(par_path, 'w') as f:
+    f.write(f'PROBLEM_FILE = {os.path.basename(tsp_path)}\n')
+    f.write(f'OUTPUT_TOUR_FILE = {os.path.basename(tsp_path).replace(".tsp", ".sol")}\n')
+    f.write('RUNS = 10\n')
 
-costo_total = 0
-for i in range(len(solucion) - 1):
-    costo_total += C[solucion[i], solucion[i+1]]
-
-plt.figure(figsize=(6, 6))
-puntos = np.random.rand(n, 2)
-for i in range(n):
-    plt.scatter(puntos[i, 0], puntos[i, 1], c='blue')
-    plt.text(puntos[i, 0]+0.01, puntos[i, 1]+0.01, str(i), fontsize=12)
-for i in range(len(solucion) - 1):
-    plt.plot([puntos[solucion[i], 0], puntos[solucion[i+1], 0]],
-             [puntos[solucion[i], 1], puntos[solucion[i+1], 1]], 'r-')
-plt.title(f'Recorrido heurístico - Costo total: {costo_total}')
-plt.show()
-
-# Mostrar secuencia y costo
-df_resultado = pd.DataFrame({'Posición': range(len(solucion)), 'Nodo': solucion})
-print(df_resultado)
-print(f"Costo total de la solución heurística: {costo_total}")
+print(f'Se generó {par_path}')
